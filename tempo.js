251e849ca8c53e6f134acc5d607525a8d348d02f
JhonnyBigoud <80720941+JhonnyBigoud@users.noreply.github.com>
2022-08-01 08:18:47 +0200

    Morning Update

--- tempo.js
+++ tempo.js
@@ -1,3 +1,37 @@
+// THE GOOD ONE !
+
+function changeHeaderImage() {
+
+    const images = [
+
+        'url("./images/bg0.jpg")',
+        'url("./images/bg1.jpg")',
+        'url("./images/bg2.jpg")',
+        'url("./images/bg3.jpg")',
+        'url("./images/bg4.jpg")',
+        'url("./images/bg5.jpg")',
+        'url("./images/bg6.jpg")',
+        'url("./images/bg7.jpg")',
+        'url("./images/bg8.jpg")',
+        'url("./images/bg9.jpg")',
+        'url("./images/bg10.jpg")',
+        'url("./images/bg11.jpg")',
+        'url("./images/bg12.jpg")',
+        'url("./images/bg13.jpg")',
+        'url("./images/bg14.jpg")',
+        'url("./images/bg15.jpg")',
+        'url("./images/bg16.jpg")',
+    ]
+
+    const header =  document.getElementById('header');
+    const bg = images[Math.floor(Math.random()*images.length)];
+    header.style.backgroundImage = bg;
+}
+
+setInterval(changeHeaderImage, 3000);
+
+// SLIDER
+
 const items = document.querySelectorAll('img.eleanor');
 const nbSlide = items.length;
 const suivant = document.querySelector('.right');
@@ -38,3 +72,16 @@ function previousSlide() {
 }
 
 precedent.addEventListener('click', previousSlide)
+
+//Changement input = date au clic
+
+function changeInputType(input) {
+    input.type = "date";
+}
+
+let inputs = document.getElementsByClassName("inputana");
+for(let i = 0; i<inputs.length; i++) {
+    inputs[i].addEventListener("focus",function(){changeInputType(this);});
+}
+
+changeInputType();
